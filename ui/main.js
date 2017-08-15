5ef8b5ee938dbc0a6e605b0fcbcebe737cddc53d
imsouravpal <30979142+imsouravpal@users.noreply.github.com>
2017-08-15 19:32:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,18 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New value";
 
 //Change2: Move the image on click
-var img = document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginleft = '500px';
-};
+//var img = document.getElementById('madi');
+//img.onclick = function() {
+//    img.style.marginleft = '500px';
+//};
 
-//Change3: 
+//Change3: Move image gradualy by using intervalTime
+var img = document.getElementById('madi');
+    marginLeft = 0;
+    function moveRight(){
+        marginLeft = marginLeft + 10;
+        img.style.marginLeft = marginLeft + 'px';
+    }
+    img.onclick = function() {
+        var interval = setInterval(moveRight, 100);
+};  
