84c19da64c34043ae2d1e1d0d4d22cfb00159f56
imsouravpal <30979142+imsouravpal@users.noreply.github.com>
2017-08-15 19:21:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,8 @@ element.innerHTML = "New value";
 
 //Change2: Move the image on click
 var element = document.getElementById('madi');
-madi.onclick = function() {
-    madi.style.marginleft = '100px';
+img.onclick = function() {
+    img.style.marginleft = '100px';
 };
 
 //Change3: 
