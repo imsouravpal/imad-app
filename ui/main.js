4d20a5f309c07298777891d53e15603200872093
imsouravpal <30979142+imsouravpal@users.noreply.github.com>
2017-08-15 19:20:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,13 @@
 console.log('Loaded!');
-//Change
+
+//Change1 : Change the main taxt region
 var element = document.getElementById('main-text');
 element.innerHTML = "New value";
+
+//Change2: Move the image on click
+var element = document.getElementById('madi');
+madi.onclick = function() {
+    madi.style.marginleft = '100px';
+};
+
+//Change3: 
