fda0bb38506bdd39065da63be648c5a75cf69947
imsouravpal <30979142+imsouravpal@users.noreply.github.com>
2017-08-20 03:28:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,48 +27,51 @@ var img = document.getElementById('madi');
 };  
 
 
+//-----------------------------------------------------------------------------------------------------------------------------------------
 
 //Counter code : Implementation Startpoint.
 
 var button = document.getElementById('counter');
-var counter=0;
+//var counter=0;
 
-button.onclick = function() {
+button.onclick = function() {   
     
     //Creat a requestObject: to the counter endpoint
-    var request = new XMLHttpRequest();     //Creat a request by using something called XMLHttp request.
+       var request = new XMLHttpRequest();     //Creat a request by using something called XMLHttp request.
       
     
     //Request are many type.Open,Sent,loading,successfully loded:Stages of Request
     //Capture the response and save it in a variable. For that, we have to do detect the change in state.
+    
     request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.Done) {     //Check the current state of Request Object.
+      if (request.readyState === XMLHttpRequest.Done) {     //Check the current state of Request object.
           
           //Take Some action if it is successful.
-          if (request.status === 200)   {           //First check if this is an Successful request or not!!
-             var counter = request.responseText;    //Extract the value from that request. Take the req and put it in the counter variable.
+            if (request.status === 200)   {           //First check if this is an Successful request or not!!
+              var counter = request.responseText;    //Extract the value from that request. Take the req and put it in the counter variabl.
              var span = document.getElementById('count');
              span.innerHTML = counter.toString();   //Take the counter variable and put it into the spam.
-          } //This is all we do Once we receve the response from the request. 
+            } //This is all we do Once we receve the response from the request. 
           
-      }   
+        }   
       
-      //If not, then not Not Done Yet.
-      
+            //If not, then not Not Done Yet.
     };
-  
+      
     //Make the request: 
-    request.open('GET', 'http://http://spsourav263.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://spsourav263.imad.hasura-app.io/counter', true);
     request.send(null);
 
   
     //Render the variable in the correct spam
-//    counter = counter+1;
-//    var span = document.getElementById('count');
-//    span.innerHTML = counter.toString();
-};  //Counter code Implementation completed.
+      //counter = counter+1;
+      //var span = document.getElementById('count');
+      //span.innerHTML = counter.toString();
+};        //Counter code Implementation completed.
 
 
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
 
 //Submit Name.
 var nameInput = document.getElementById('name');
