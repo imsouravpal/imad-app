15e17cabc1910f8c7eac71211a07ae77877f4e32
imsouravpal <30979142+imsouravpal@users.noreply.github.com>
2017-08-20 21:29:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -84,7 +84,7 @@ submit.onclick = function() {
        //Convert this into Html-String
     var list = '';
     for (var i=0; i<names.length; i++) {
-        list += '<li>' + name[i] + '</li>';
+        list += '<li>' + names[i] + '</li>';
   }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
