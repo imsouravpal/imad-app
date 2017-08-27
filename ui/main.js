0e11a2cbb913da35057d651c5f2b9398e261200c
imsouravpal <30979142+imsouravpal@users.noreply.github.com>
2017-08-27 17:52:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,35 +31,35 @@ console.log('Loaded!');
 
 //Counter code : Implementation Startpoint.
 
-var button = document.getElementById('counter');
+//var button = document.getElementById('counter');
 //var counter=0;
 
-button.onclick = function() {   
+//button.onclick = function() {   
     
     //Creat a requestObject: to the counter endpoint
-    var request = new XMLHttpRequest();     //Creat a request by using XMLHttp request.
+    //var request = new XMLHttpRequest();     //Creat a request by using XMLHttp request.
       
     
     //Request are many types. Open,Sent,loading,successfully loded:Stages of Request
     //Capture the response and save it in a variable. For that, we have to do:- detect the change in state.
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.Done) {     //Check the current state of Request object.
+    //request.onreadystatechange = function() {
+      //if (request.readyState === XMLHttpRequest.Done) {     //Check the current state of Request object.
           
           //Take Some action if it is successful.
-            if (request.status === 200)   {           //First check if this is an Successful request or not!!
-              var counter = request.responseText;    //Extract the value from that request. Take the req and put it in the counter variabl.
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();   //Take the counter variable and put it into the spam.
-            } //This is all we do Once we receve the response from the request. 
+            //if (request.status === 200)   {           //First check if this is an Successful request or not!!
+              //var counter = request.responseText;    //Extract the value from that request. Take the req and put it in the counter variabl.
+              //var span = document.getElementById('count');
+              //span.innerHTML = counter.toString();   //Take the counter variable and put it into the spam.
+            //} //This is all we do Once we receve the response from the request. 
           
-        }   
+        //}   
       
             //If not, then not Not Done Yet.
-    };
+    //};
       
     //Make the request: 
-    request.open('GET', 'http://spsourav263.imad.hasura-app.io/counter', true);
-    request.send(null);
+    //request.open('GET', 'http://spsourav263.imad.hasura-app.io/counter', true);
+    //request.send(null);
 
   
     //Render the variable in the correct spam
@@ -73,6 +73,62 @@ button.onclick = function() {
 //-----------------------------------------------------------------------------------------------------------------------------------------
 
 //Submit Name.
+
+//var submit = document.getElementById('submit_btn');  //When ever the submit-btn is clicked
+//submit.onclick = function() {
+  //Creat a Request Object
+  //var request = new XMLHttpRequest();     //Creat a request by using XMLHttp request.
+      
+    
+    //Request are many types. Open,Sent,loading,successfully loded:Stages of Request
+    //Capture the response and save it in a variable. For that, we have to do:- detect the change in state.
+    //request.onreadystatechange = function() {
+      //if (request.readyState === XMLHttpRequest.Done) {     //Check the current state of Request object.
+          
+          //Take Some action if it is successful.
+            //if (request.status === 200)   {           
+                //Capture the list of name and render it as a list.
+                //var names = ['name1', 'name2', 'name3']; //IF REQ SUCCESSFUL get the req of names.
+                //Insted of hardcoding the list of names. Get that from response
+                //var names = request.responseText; //Is a string not a js array. 
+                //names = JSON.parse(names);  //Converted from string back into a Object. Here its an array.
+                //Convert this into Html-String
+                //var list = '';
+                //for (var i=0; i< names.length; i++) {
+                    //list += '<li>' + names[i] + '</li>';
+                //}
+                //var ul = document.getElementById('namelist');  //Render the list of names.
+                //ul.innerHTML = list;
+            //} //This is all we do Once we receve the response from the request. 
+          
+        //}   
+      
+            //If not, then not Not Done Yet.
+    //};
+      
+    //Make the request: 
+    //var nameInput = document.getElementById('name');  //Select InputBox
+    //var name = nameInput.value;  //Extrect the value from input box
+    //request.open('GET', 'http://spsourav263.imad.hasura-app.io/submit-name?name=' + name, true); //Send the req to this URL.
+    //request.send(null);
+ 
+  //Capture the list of name and render it as a list.
+    //var names = ['name1', 'name2', 'name3'];
+       //We use this part of code inside when req is successfull. in submit.onclick function. 
+       //Convert this into Html-String
+    //var list = '';
+    //for (var i=0; i<names.length; i++) {
+        //list += '<li>' + names[i] + '</li>';
+  //}
+    //var ul = document.getElementById('namelist');
+    //ul.innerHTML = list;
+//};
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+//Submit username/password to login.
 
 var submit = document.getElementById('submit_btn');  //When ever the submit-btn is clicked
 submit.onclick = function() {
@@ -88,18 +144,13 @@ submit.onclick = function() {
           //Take Some action if it is successful.
             if (request.status === 200)   {           
                 //Capture the list of name and render it as a list.
-                //var names = ['name1', 'name2', 'name3']; //IF REQ SUCCESSFUL get the req of names.
-                //Insted of hardcoding the list of names. Get that from response
-                var names = request.responseText; //Is a string not a js array. 
-                names = JSON.parse(names);  //Converted from string back into a Object. Here its an array.
-                //Convert this into Html-String
-                var list = '';
-                for (var i=0; i< names.length; i++) {
-                    list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');  //Render the list of names.
-                ul.innerHTML = list;
-            } //This is all we do Once we receve the response from the request. 
+                console.log('user logged in');
+                alert('Logged in successfully');
+            } else if (request.status === 403){
+                alert('Username/Password is incorrect');
+            } else if (request.status === 500){
+                alert('Something went wrong on the server!');
+            }
           
         }   
       
@@ -107,19 +158,11 @@ submit.onclick = function() {
     };
       
     //Make the request: 
-    var nameInput = document.getElementById('name');  //Select InputBox
-    var name = nameInput.value;  //Extrect the value from input box
-    request.open('GET', 'http://spsourav263.imad.hasura-app.io/submit-name?name=' + name, true); //Send the req to this URL.
-    request.send(null);
- 
-  //Capture the list of name and render it as a list.
-    //var names = ['name1', 'name2', 'name3'];
-       //We use this part of code inside when req is successfull. in submit.onclick function. 
-       //Convert this into Html-String
-    //var list = '';
-    //for (var i=0; i<names.length; i++) {
-        //list += '<li>' + names[i] + '</li>';
-  //}
-    //var ul = document.getElementById('namelist');
-    //ul.innerHTML = list;
+    var username = document.getElementById('username').value;  //username is extract from inputElement as soon as button is clicked.
+    var password = document.getElementById('password').value;  //password is extract from inputElement as soon as button is clicked.
+    console.log(username); //for debuging purpose this two logs are used here.
+    console.log(password);
+    request.open('POST', 'http://spsourav263.imad.hasura-app.io/login', true); //Send the req to this URL.
+    request.setRequestHeader('Content-Type:', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
